daff77dee4d0db79126b5b04fb64cc26c7836844
cy <cy@shctis.com>
2020-09-15 17:21:16 +0800

    解决跨域问题

--- leyou-item/leyou-item-service/src/main/java/com/leyou/LeyouItemApplication.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/LeyouItemApplication.java
@@ -3,6 +3,7 @@ package com.leyou;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @Author zzx
@@ -10,6 +11,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient //eureka客户端注解
+@MapperScan("com.leyou.item.mapper") //开启通用mapper的扫描并且配置路径
 public class LeyouItemApplication {
     public static void main(String[] args) {
         SpringApplication.run(LeyouItemApplication.class, args);
---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/CategoryController.java
@@ -0,0 +1,47 @@
+package com.leyou.item.controller;
+
+import com.leyou.item.pojo.Category;
+import com.leyou.item.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collections;
+import java.util.List;
+
+@Controller
+@RequestMapping("category") //配置全局路径为category
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 根据父节点id查询子节点
+     * @param pid 父节点的id
+     * @return
+     */
+    @GetMapping("list")
+    public ResponseEntity<List<Category>> queryCategoriesByPid(@RequestParam(value = "pid", defaultValue = "0") Long pid) {
+        if (pid == null || pid < 0) {
+            //参数不合法：400 返回BAD_REQUEST
+            // 多种写法
+            // return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            // return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return ResponseEntity.badRequest().build();
+        }
+        List<Category> categories = this.categoryService.queryCategoriesByPid(pid);
+        if (CollectionUtils.isEmpty(categories)) {
+            //查询到的结果为空：404 资源服务器未找到 返回NOT_FOUND
+            // return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            return ResponseEntity.notFound().build();
+        }
+        //查询成功：200 ok返回数据
+        return ResponseEntity.ok(categories);
+    }
+}
