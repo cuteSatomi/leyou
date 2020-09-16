6c4c87d3352638956d457be458c60dd08c58b425
cy <cy@shctis.com>
2020-09-16 12:05:11 +0800

    品牌搜索分页查询完成

---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/BrandService.java
@@ -0,0 +1,60 @@
+package com.leyou.item.service;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.leyou.common.pojo.PageResult;
+import com.leyou.item.mapper.BrandMapper;
+import com.leyou.item.pojo.Brand;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
+
+@Service
+public class BrandService {
+
+    @Autowired
+    private BrandMapper brandMapper;
+
+    /**
+     * 根据查询条件分页并排序查询品牌信息
+     *
+     * @param key    查询的条件
+     * @param page   查询的页数
+     * @param rows   每页显示条数
+     * @param sortBy 根据哪个字段排序
+     * @param desc   是否升序
+     * @return 返回分类集合
+     */
+    public PageResult<Brand> queryBrandsByPage(String key, Integer page, Integer rows, String sortBy, Boolean desc) {
+
+        //初始化Example对象，复杂的条件查询
+        Example example = new Example(Brand.class);
+        Example.Criteria criteria = example.createCriteria();
+
+        //根据name模糊查询，或者根据首字母查询
+        //key不为空才处理是否进行模糊查询
+        if (StringUtils.isNotBlank(key)) {
+            // andLike是根据name来模糊查询，orEquals根据首字母来查询，两个条件应该是or的关系
+            criteria.andLike("name", "%" + key + "%").orEqualTo("letter", key);
+        }
+        //添加分页条件，使用PageHelper插件
+        PageHelper.startPage(page, rows);
+
+        //添加排序条件
+        //排序条件不为空时，才进行处理
+        if (StringUtils.isNotBlank(sortBy)) {
+            // desc参数为true时，才为降序
+            example.setOrderByClause(sortBy + " " + (desc ? "desc" : "asc"));
+        }
+
+        //进行查询
+        List<Brand> brands = this.brandMapper.selectByExample(example);
+        //将查询的对象包装成PageInfo分页对象
+        PageInfo<Brand> pageInfo = new PageInfo<>(brands);
+        //包装成分页结果集返回
+        return new PageResult<>(pageInfo.getTotal(),pageInfo.getList());
+    }
+}
